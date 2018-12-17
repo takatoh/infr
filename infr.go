ae9587262205e051114260a1a2205bd2892a0901
takatoh <takatoh.m@gmail.com>
2018-12-17 09:47:01 +0900

    infr.go: Implement function `integrate'.

--- infr.go
+++ infr.go
@@ -1,6 +1,9 @@
 package infr
 
 import (
+	"math"
+	"math/cmplx"
+
 	"github.com/takatoh/fft"
 )
 
@@ -31,5 +34,19 @@ func Integrate(x []float64, n int) []float64 {
 }
 
 func integrate(c []complex128, nn int) []complex128 {
-	return c
+	s := make([]complex128, nn)
+	nfold := nn / 2
+	pn := math.Pi / float64(nn)
+
+	c0 := real(c[0])
+	s0 := float64(nn) / 2.0 * c0 * pn
+	for k := 1; k < nfold - 1; k++ {
+		s0 = s0 - imag(c[k]) / float64(k)
+		s[k] = complex(-1.0 * c0 * pn, c0 * pn / math.Tan(float64(k) * pn)) - complex(0.0, 1.0 / float64(k)) * c[k]
+		s[nn - k] = cmplx.Conj(s[k])
+	}
+	s[0] = complex(s0 * 2.0, 0.0)
+	s[nfold] = complex(-1.0 * c0 * pn, 0.0)
+
+	return s
 }

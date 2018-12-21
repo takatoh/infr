30441761bafd1ce810f243fecb0b51cc196a7657
takatoh <takatoh.m@gmail.com>
2018-12-21 15:41:56 +0900

    cmd/infr-example/example.go: Update.

--- cmd/infr-example/example.go
+++ cmd/infr-example/example.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/takatoh/fft"
 	"github.com/takatoh/infr"
 	"github.com/takatoh/infr/cmd/infr-example/wave"
 )
@@ -12,10 +13,25 @@ func main() {
 	csvfile := os.Args[1]
 	w := wave.LoadCSV(csvfile)
 	acc := w.Data
+	n := len(acc)
 
-	vel := infr.Integrate(acc, len(acc))
-	n := len(vel)
+	nn := 2
+	for nn < n {
+		nn *= 2
+	}
+
+	var c []complex128
 	for i := 0; i < n; i++ {
-		fmt.Printf("%.3f\n", vel[i])
+		c = append(c, complex(acc[i], 0.0))
+	}
+	for i := n; i < nn; i++ {
+		c = append(c, complex(0.0, 0.0))
+	}
+	cf := fft.FFT(c, nn)
+	cf = infr.Integrate(cf, nn)
+	c = fft.IFFT(cf, nn)
+
+	for i := 0; i < n; i++ {
+		fmt.Printf("%.3f\n", real(c[i]))
 	}
 }

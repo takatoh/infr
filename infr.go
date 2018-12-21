990e36e3b1185073a8cf0304b1fc39bc7a17256b
takatoh <takatoh.m@gmail.com>
2018-12-21 17:05:01 +0900

    infr.go: Re-implement function `Integrate'.

--- infr.go
+++ infr.go
@@ -5,20 +5,21 @@ import (
 	"math/cmplx"
 )
 
-func Integrate(c []complex128, nn int) []complex128 {
+func Integrate(c []complex128, nn int, dt, v0 float64) []complex128 {
 	s := make([]complex128, nn)
 	nfold := nn / 2
 	pn := math.Pi / float64(nn)
 
-	c0 := real(c[0])
-	s0 := float64(nn) / 2.0 * c0 * pn
-	for k := 1; k < nfold - 1; k++ {
-		s0 = s0 - imag(c[k]) / float64(k)
-		s[k] = complex(-1.0 * c0 * pn, c0 * pn / math.Tan(float64(k) * pn)) - complex(0.0, 1.0 / float64(k)) * c[k]
+//	c0 := real(c[0])
+	cr := 2.0 * pn * v0 / dt
+	for k := 1; k < nfold; k++ {
+		cr = cr - 2.0 * imag(c[k]) / float64(k)
+		s[k] = complex(pn, 0.0) * c[0] * complex(-1.0, 1.0 / math.Tan(float64(k) * pn)) -
+			complex(0.0, 1.0) * c[k] / complex(float64(k), 0.0)
 		s[nn - k] = cmplx.Conj(s[k])
 	}
-	s[0] = complex(s0 * 2.0, 0.0)
-	s[nfold] = complex(-1.0 * c0 * pn, 0.0)
+	s[0] = complex(cr, 0.0) * complex(pn * float64(nn - 1), 0.0) * c[0]
+	s[nfold] = complex(-1.0 * pn, 0.0) * c[0]
 
 	return s
 }

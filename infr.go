04c2a4d4e4d659c2557f3b9074514f0dd38663d2
takatoh <takatoh.m@gmail.com>
2018-12-21 15:40:58 +0900

    infr.go: Remove function `Integrage', them rename function `integrate' to `Integrate'.

--- infr.go
+++ infr.go
@@ -3,37 +3,9 @@ package infr
 import (
 	"math"
 	"math/cmplx"
-
-	"github.com/takatoh/fft"
 )
 
-func Integrate(x []float64, n int) []float64 {
-	nn := 2
-	for nn < n {
-		nn *= 2
-	}
-
-	var c []complex128
-	for i := 0; i < n; i++ {
-		c = append(c, complex(x[i], 0.0))
-	}
-	for i := n; i < nn; i++ {
-		c = append(c, complex(0.0, 0.0))
-	}
-
-	cf := fft.FFT(c, nn)
-	cf = integrate(cf, nn)
-	c = fft.IFFT(cf, nn)
-
-	var y []float64
-	for i := 0; i < n; i++ {
-		y = append(y, real(c[i]))
-	}
-
-	return y
-}
-
-func integrate(c []complex128, nn int) []complex128 {
+func Integrate(c []complex128, nn int) []complex128 {
 	s := make([]complex128, nn)
 	nfold := nn / 2
 	pn := math.Pi / float64(nn)

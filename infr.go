914b2a3ebc411bf7ec3a82f28c7b2733fed2a254
takatoh <takatoh.m@gmail.com>
2019-01-11 17:41:55 +0900

    infr.go: Fix bug.

--- infr.go
+++ infr.go
@@ -10,16 +10,19 @@ func Integrate(c []complex128, nn int, dt, v0 float64) []complex128 {
 	nfold := nn / 2
 	pn := math.Pi / float64(nn)
 
-//	c0 := real(c[0])
 	cr := 2.0 * pn * v0 / dt
 	for k := 1; k < nfold; k++ {
 		cr = cr - 2.0 * imag(c[k]) / float64(k)
 		s[k] = complex(pn, 0.0) * c[0] * complex(-1.0, 1.0 / math.Tan(float64(k) * pn)) -
-			complex(0.0, 1.0) * c[k] / complex(float64(k), 0.0)
+			complex(0.0, 1.0 / float64(k)) * c[k]
 		s[nn - k] = cmplx.Conj(s[k])
 	}
-	s[0] = complex(cr, 0.0) * complex(pn * float64(nn - 1), 0.0) * c[0]
+	s[0] = complex(cr, 0.0) + complex(pn * float64(nn - 1), 0.0) * c[0]
 	s[nfold] = complex(-1.0 * pn, 0.0) * c[0]
+
+	for k := 0; k < nn; k++ {
+		s[k] = complex(float64(nn) * dt / (2.0 * math.Pi), 0.0) * s[k] 
+	}
 
 	return s
 }
